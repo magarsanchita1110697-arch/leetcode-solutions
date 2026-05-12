8cdd1e7d9746a57c2cf56bc7131ba420c0956ceb
magarsanchita1110697-arch <magarsanchita1110697@gmail.com>
2026-05-12 22:23:10 +0530

    Added solution to 1365

---
+++ 1365-how-many-numbers-smaller-than-current-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+
+       int n = nums.size();
+       vector<int>result(n);
+
+       for(int i=0; i<n; i++){
+        int count=0;
+        int j;
+        for(j=0; j<n; j++){
+           if(j != i && nums[j]<nums[i])
+           count++;
+        }
+        result [i]=count;
+       }
+       return result;
+    }
+};
